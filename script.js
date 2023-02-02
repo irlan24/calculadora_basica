4f519fe5ede426c3df70ca89936f16f3369f9616
Irlan Nonato <irlan.nonato97@hotmail.com>
2023-02-02 13:44:50 -0300

    Iniciando criação dos temas

--- script.js
+++ script.js
@@ -23,3 +23,28 @@ function res(){
 
     visor.innerHTML = eval(visor.innerHTML)
 }
+
+/* DESENVOLVIMENTO DOS TEMAS */
+    let container = document.querySelector('.container')
+    let nav = document.querySelector('nav')
+    let footer = document.querySelector('footer')
+    let h1 = document.querySelector('h1')
+    let tabela = document.querySelector('table')
+    
+
+function tema2(){
+    
+    container.style.backgroundColor = "#206343d2"
+    nav.style.backgroundColor = "#206343d2"
+    footer.style.backgroundColor = "#206343d2"
+    h1.style.color = '#206343d2'
+    tabela.style.backgroundColor = '#4AE89Bc0'
+    
+}
+function tema1(){
+    container.style.backgroundColor = "#526d70d2"
+    nav.style.backgroundColor = "#526d70d2"
+    footer.style.backgroundColor = "#526d70d2"
+    h1.style.color = '#526d70d2'
+    tabela.style.backgroundColor = '#6ce9fac0'
+}
